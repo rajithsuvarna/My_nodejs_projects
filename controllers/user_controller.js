8817c8b4d8f90374e1e5c29058e32c84ee97b3e5
rajithsuvarna <rajithsuvarna49@gmail.com>
2023-08-17 10:10:43 +0530

    static file access with layouts

--- controllers/user_controller.js
+++ controllers/user_controller.js
@@ -1,3 +1,3 @@
 module.exports.profile = function (req, res) {
-  return res.end("<h1>USER Router</h1>");
+  return res.render("userprofile", { title: "User Profile" });
 };
--- index.js
+++ index.js
@@ -2,6 +2,13 @@ const express = require("express");
 const port = 8000;
 
 const app = express();
+app.use(express.static("./assets"));
+const expressLayouts = require("express-ejs-layouts");
+app.use(expressLayouts);
+
+app.set("layout extractStyles", true);
+app.set("layout extractScripts", true);
+
 app.use("/", require("./routes"));
 
 app.set("view engine", "ejs");
