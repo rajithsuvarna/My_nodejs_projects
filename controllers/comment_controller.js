13e73189911c0e7a05bbd5c406f8f129fc0e6ae9
rajithsuvarna <rajithsuvarna49@gmail.com>
2023-08-25 16:52:18 +0530

    adding and deleting post using ajax

--- controllers/comment_controller.js
+++ controllers/comment_controller.js
@@ -12,6 +12,17 @@ module.exports.create = async function (req, res) {
       });
       post.comments.push(comment);
       post.save();
+      if (req.xhr) {
+        // Similar for comments to fetch the user's id!
+        comment = await comment.populate("user", "name");
+        return res.status(200).json({
+          data: {
+            comment: comment,
+          },
+          message: "Post created!",
+        });
+      }
+
       req.flash("success", "Comment added");
       res.redirect("/");
     }
@@ -34,6 +45,16 @@ module.exports.destroy = async function (req, res) {
       let post = await Post.findByIdAndUpdate(postId, {
         $pull: { comments: req.params.id },
       });
+
+      if (req.xhr) {
+        return res.status(200).json({
+          data: {
+            comment_id: req.params.id,
+          },
+          message: "Post deleted",
+        });
+      }
+
       req.flash("success", "Comment deleted");
 
       return res.redirect("back");
--- controllers/post_controller.js
+++ controllers/post_controller.js
@@ -3,7 +3,22 @@ const comment = require("../models/comment");
 
 module.exports.create = async function (req, res) {
   try {
-    await post.create({ content: req.body.content, user: req.user._id });
+    let Post = await post.create({
+      content: req.body.content,
+      user: req.user._id,
+    });
+
+    if (req.xhr) {
+      Post = await Post.populate("user", "name");
+
+      return res.status(200).json({
+        data: {
+          post: Post,
+        },
+        message: "Post Created",
+      });
+    }
+
     req.flash("success", "Post published");
     return res.redirect("back");
   } catch (err) {
@@ -19,6 +34,16 @@ module.exports.destroy = async function (req, res) {
     if (Post.user == req.user.id) {
       Post.deleteOne();
       await comment.deleteMany({ post: req.params.id });
+
+      if (req.xhr) {
+        return res.status(200).json({
+          data: {
+            post_id: req.params.id,
+          },
+          messae: "Post deleted",
+        });
+      }
+
       req.flash("success", "Post and associated comments deleted");
       return res.redirect("back");
     } else {
